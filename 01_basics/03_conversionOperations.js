63c85369cf450278ae5a589034cf5fa27e182f4c
Ekta7873 <90027227+Ekta7873@users.noreply.github.com>
2024-07-31 08:51:20 +0000

    conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,2 +1,13 @@
 let number = 21
-console.log(String(number));
+let stringNum = String(number);
+console.log(stringNum);
+console.log(typeof stringNum);
+
+
+let isTrue = 1
+let booleanType = Boolean(isTrue)
+console.log(typeof isTrue);
+
+let name="Ekta7873"
+let alphaNum = Number(name)
+console.log(alphaNum); // Not a number because characters and numbers are both included
