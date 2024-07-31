bbd72a09f0579ac58bd0997f4891f3882cc15df5
Ekta7873 <90027227+Ekta7873@users.noreply.github.com>
2024-07-31 07:27:15 +0000

    dataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,8 @@
-const 
+const accountId = 123456;
+let accountEmail="abs@gmail.com"  
+var accountPassword = "12345"
+accountCity = "Bengluru"
+let accountState
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // Treat all js code as newer version
+
+
+let name ="Ekta"
+let age = 22
+let isFemale = true
+
+/*
+number-> 2 to power 53
+bigint
+string =""
+boolean true/false
+undefined
+symbol--unique elements define krne k liye
+null -- Standalone value hoti and ye object type hota hai
+*/
+
+console.log(typeof age);
